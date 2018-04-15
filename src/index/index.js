89985a57fe08b4fa8c2e9819ae6c1d495328ebaf
ChillyWilly01 <32175084+ChillyWilly01@users.noreply.github.com>
2018-04-15 21:42:15 +0300

    Added Webpack files

---
+++ src/index/index.js
@@ -0,0 +1,24 @@
+import './index.scss';
+
+// const name = prompt('Enter name: ', '');
+// const secretName = 'admin';
+//
+// if (name ===secretName) {
+//     const password = prompt('Enter admin\'s password');
+//     if(password === '123'){
+//         console.log('Logged in');
+//     } else {
+//         console.log('Password is wrong!');
+//     }
+//
+// } else{
+//     console.log('Good buy');
+// }
+
+// let person = {
+//     name: prompt('Enter name'),
+//     age: 26
+// };
+//
+// const phrase = `Hello, my name is ${person.name} . Age ${person.age}`;
+// console.log(phrase);
